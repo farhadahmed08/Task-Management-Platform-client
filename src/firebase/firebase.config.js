c4193b10126f2245e5ffb76b5667c222f42fea05
Md Farhad Ahmed <farhaddhkbd08@gmail.com>
2023-12-22 16:03:48 +0800

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCBeA5jaItZerFBOYOKySMnnDFfMRP13VQ",
+  authDomain: "task-management-platform-d0848.firebaseapp.com",
+  projectId: "task-management-platform-d0848",
+  storageBucket: "task-management-platform-d0848.appspot.com",
+  messagingSenderId: "599764438165",
+  appId: "1:599764438165:web:5b8ac078d9212dfc0adf9c"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
